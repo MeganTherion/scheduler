64314b029be584c5ebb63bb44dc2fe878e9a87b3
Megan Therion <meganhutt@hotmail.com>
2022-07-16 19:28:05 -0700

    display appointments

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,10 +1,20 @@
-import React from "react";
-import Appointment from "components/Appointment";
-import "components/Appointment/styles.scss";
-import classNames from "classnames";
+import React, { Fragment } from "react";
 
-export default function Application(props) {
+import "components/Appointment/styles.scss";
+
+import Header from "components/Appointment/Header";
+import Show from "components/Appointment/Show";
+import Empty from "components/Appointment/Empty";
+export default function Appointment(props) {
   return(
-    <article className="appointment" time={props.time}></article>
+    props.interview ? 
+    <article className="appointment">
+      <Header time={props.time} />
+      <Show student={props.interview.student} interviewer={props.interview.interviewer}/>
+     </article> :
+     <article className="appointment">
+      <Header time={props.time}/>
+      <Empty />
+    </article>
   );
 };
