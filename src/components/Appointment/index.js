854e3a0fdd76367838baf84a72d0478f5f4cc5af
Megan Therion <meganhutt@hotmail.com>
2022-07-26 09:12:56 -0700

    fix custom hooks functionality

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,19 +1,38 @@
 import React, { Fragment } from "react";
+import axios from "axios";
 import "components/Appointment/styles.scss";
-import Header from "components/Appointment/Header";
-import Show from "components/Appointment/Show";
-import Empty from "components/Appointment/Empty";
+import Header from "./Header";
+import Show from "./Show";
+import Empty from "./Empty";
+import Form from "./Form";
+import useVisualMode from "hooks/useVisualMode";
+const EMPTY = "EMPTY";
+const SHOW = "SHOW";
+const CREATE = "CREATE";
 
 export default function Appointment(props) {
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
   return(
-    props.interview ? 
+     
     <article className="appointment">
-      <Header time={props.time} />
-      <Show student={props.interview.student} interviewer={props.interview.interviewer}/>
-     </article> :
-     <article className="appointment">
-      <Header time={props.time}/>
-      <Empty />
-    </article>
+      <Header 
+      time={props.time} />
+    {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+{mode === SHOW && (
+  <Show
+    student={props.interview.student}
+    interviewer={props.interview.interviewer}
+  />
+)}
+    {mode === CREATE && (
+      <Form
+      interviewers = {[]}
+      onCancel ={() => back()}
+      />
+    )} 
+     </article> 
+     
   );
 };
